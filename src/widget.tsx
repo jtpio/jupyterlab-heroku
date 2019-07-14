9dd89e9d2c863fbbeee3285756c64ebb4244c604
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-15 00:12:06 +0200

    Add deploy button

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,16 +1,32 @@
-import * as React from "react";
+import { ToolbarButton, Toolbar } from "@jupyterlab/apputils";
 
-import { ReactWidget } from "@jupyterlab/apputils";
+import { Widget, PanelLayout } from "@phosphor/widgets";
 
 const HEROKU_WIDGET_CLASS = "jp-Heroku";
+const HEROKU_PUSH_CLASS = "jp-FileUploadIcon";
 
-export class HerokuWidget extends ReactWidget {
+export class HerokuWidget extends Widget {
   constructor() {
     super();
     this.addClass(HEROKU_WIDGET_CLASS);
+
+    this.toolbar = new Toolbar<Widget>();
+
+    let deployButton = new ToolbarButton({
+      iconClassName: HEROKU_PUSH_CLASS,
+      onClick: () => {
+        console.log("DEPLOY");
+      },
+      tooltip: "Deploy To Heroku"
+    });
+
+    this.toolbar.addItem("deploy", deployButton);
+
+    let layout = new PanelLayout();
+    layout.addWidget(this.toolbar);
+
+    this.layout = layout;
   }
 
-  render() {
-    return <div>Heroku</div>;
-  }
+  readonly toolbar: Toolbar<Widget>;
 }
