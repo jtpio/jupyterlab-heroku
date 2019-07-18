b7c8f0e19bef99eb19aeafdc9f7b6246931220b2
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-18 17:37:52 +0200

    Add button to show app logs in a terminal

--- src/commands.ts
+++
@@ -1,7 +0,0 @@
-import { JupyterFrontEnd } from "@jupyterlab/application";
-
-export namespace CommandIDs {
-  export const herokuCreate = "heroku:create";
-}
-
-export function addCommands(app: JupyterFrontEnd) {}
